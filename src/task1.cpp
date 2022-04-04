430db0b3c3c0a91a197d36f86a15b8098e75f31a
nekromantb <nekromantbit@gmail.com>
2022-04-04 21:07:12 +0300

    initial commit

---
+++ src/task1.cpp
@@ -0,0 +1,17 @@
+#include "task1.h"
+#include <iostream>
+#include <assert.h>
+#include <map>
+
+class Branch
+{
+    bool parent = 0;
+    std::string nameElf = "None";
+};
+
+
+
+void task1()
+{
+
+}
